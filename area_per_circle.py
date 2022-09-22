30cf81faceee7eaa87bf103ef76fbdf3847f8d15
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-22 01:43:52 +0000

    I made a program that measures the area and circumference of a circle

---
+++ area_per_circle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Date: 09.21.22
+# This program calculates the area and circumference of a circle.
+#with the radius of 15 mm
+
+import math
+from turtle import radians
+
+rad = 15
+Circumference = math.pi * rad * 2
+Area = math.pi *rad **2
+
+print(Circumference)
+print(Area)
