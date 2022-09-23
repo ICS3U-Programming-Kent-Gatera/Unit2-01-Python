8d4065f76329d8d03509c8bdfc225b54a7ba8ccd
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-23 00:03:49 +0000

    I fixed the program a bit.

--- area_per_circle.py
+++ area_per_circle.py
@@ -2,14 +2,13 @@
 # Created By: Kent Gatera
 # Date: 09.21.22
 # This program calculates the area and circumference of a circle.
-#with the radius of 15 mm
+# with the radius of 15 mm
 
 import math
-from turtle import radians
 
 rad = 15
 Circumference = math.pi * rad * 2
-Area = math.pi *rad **2
+Area = math.pi * rad ** 2
 
-print(Circumference)
-print(Area)
+print("The circumference is {} cm". format(Circumference))
+print("The area is {} cm^2". format(Area))
